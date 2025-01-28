c0528c3845fe8d6e59f1f57a2c7c9344399644ea
Bryan Thiam <bmthia25@colby.edu>
2025-01-28 18:06:12 -0500

    Update FinalProject.ino
    Current code for Final Project

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,1 +1,181 @@
+//Including needed Libraries
+#include <Adafruit NeoPixel.h> //NeoPixel Ring
+#include <SD .h> //SD card
 
+#define D2 2
+#define NUMPIXELS 16
+
+//FSR Global Vars
+const int FSR_PINS[4] = {A0, A1, A2, A3};
+//A0 -> indices 0-2
+//A1 -> indices 4-6
+//A2 -> indices 8-10
+// A3 -> indices 12-14
+
+const int NUM_FSR = 4;
+int FORCE_THRESHOLD = 200; // Detects whether force is significant
+
+// Var for Arduino's built-in LED Pin
+const int LED_PIN = 13;
+
+//NeoPixel Ring Global Vars
+int index = 0;
+Adafruit_NeoPixel strip(NUMPIXELS, D2, NEO_GRB + NEO_KHZ800);
+
+// SD Card Vars
+const int sd_CS_PIN = 10; // CS Pin for SD Card
+File outfile; // File Object for logging data
+
+// Buzzer Definitions
+const int BUZZER_PINS[4] = {6, 7, 8, 9};
+
+// Sound frequencies for each color
+const int RED_TONE = 440;
+const int GREEN_TONE = 660;
+
+// Timing definitions 
+unsigned lon previousLogTime = 0; // Tracks time passed for logging
+const unsigned long logInterval = 1000; // Interval for logging (in milliseconds)
+
+// Array for FSR to Neopixel mapping (each FSR controls 4 Neopixels)
+const int FSR_INDICES[4][3] = {
+  {0, 1, 2},
+  {3, 4, 5},
+  {6, 7, 8},
+  {9, 10, 11}
+};
+
+// Traffic Light States
+enum TrafficStates { RED, GREEN};
+TrafficState current[4];
+
+// Timer Definitions for each FSR group
+unsigned long stateChangeTime[4] = {0, 0, 0, 0}; // Tracks when state was last changed
+const unsigned long waitThreshold = 20000; // 20 seconds threshold
+
+void setup() {
+
+  Serial.begin(9600);
+
+  // SD Card
+  pinMode(SD_CS_PIN, OUTPUT);
+  Serial.println("Initializing SD Card...");
+
+  if (!SD.begin(SD_CS_PIN)) {
+    Serial.println("Something went wrong with the SD Card!");
+    while (1); // Stops running if SD card fails to initialize
+  }
+  Serial.println("SD card initialized successfully.");
+
+  OutFile = SD.open("results.txt", FILE_WRITE);
+  if (!OutFile) {
+    Serial.println("Something went wrong with the SD Card File!");
+    while (1); // Stops running if file doesn't open
+  }
+  Serial.println("Log file opened successfully.");
+
+
+  //NeoPixel Ring Section
+  strip.begin();
+  strip.show();
+  strip.setBrightness(25);
+
+  // FSR Pins
+  for (int i = 0; o < NUM_FSR; i++) {
+    pinMode(FSR_PINS[i], INPUT);
+  }
+
+  // Built-in LED
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  // Buzzer Pins
+  for (int i = 0; i < NUM_FSR; i++) {
+    pinMode(BUZZER_PINS[i], OUTPUT);
+    noTone(BUZZER_PINS[i]);
+  }
+
+  // Traffic Light States
+  for (int i = 0; i < NUM_FSR; i++) {
+    currentState[i] = RED; // Starts with RED light
+    setPizelColor(i, strip.Color(255, 0, 0)); // Sets Neopixels to Red
+  }
+  strip.show();
+
+}
+
+void loop() {
+  // Var to track if any FSR exceeds the threshold value
+  bool anyForceDetected = false;
+  // Array to store FSR readings
+  int fsrReadings[NUM_FSR];
+
+  Serial.println("FSR Readings:");
+  for (int i = 0; i < NUM_FSR; i++) {
+      fsrReadings[i] = analogRead(FSR_PINS[i]);
+      Serial.print("FSR ");
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.println(fsrReadings[i]); 
+
+      // Checks if current FSR value exceeds the force thresold value
+      if (fsrReadings[i] > FORCE_THRESHOLD) {
+          anyForceDetected = true;
+          handleTrafficState(i, GREEN);
+      } else {
+          handleTrafficState(i, RED);
+      }
+  }
+
+  // Built-in LED is controlled based on whether it detects any force
+  if (anyForceDetected) {
+      // Turns on
+      digitalWrite(LED_PIN, HIGH); 
+      Serial.println("Force was detected on at least one FSR!");
+  } else {
+      // Turns off
+      digitalWrite(LED_PIN, LOW);
+  }
+
+  Serial.println("------------------------");
+
+  // Gets current time for logging
+  unsigned long currentMillis = millis();
+
+  // Checks to see if it's time to log data
+  if (currentMillis - previousLogTime >= logInterval) {
+      // Updates last log time
+      previousLogTime = currentMillis;
+
+      // Creates a timestamp
+      unsigned long timestamp = currentMillis;
+
+      // Sends FSR readings to SD card
+      outFile.print("Timestamp: ");
+      outFile.print(timestamp);
+      outFile.print(" ms, FSR Readings: ");
+      for (int i = 0; i < NUM_FSR; i++) {
+          outFile.print(fsrReadings[i]);
+          if (i < NUM_FSR - 1) {
+              outFile.print(", ");
+          }
+      }
+      outFile.println();
+
+      // data is written to SD card
+      OutFile.flush();
+      Serial.println("FSR readings logged to SD card.");
+
+  }  
+  delay(200);
+}
+
+void setPixelColor(int groupIndex, uint32_t color) {
+    for(int i = 0; i < 4; i++) {
+        int pixelIndex = FSR_INDICES[groupIndex][i];
+        if (pixelIndex >= 0 && pixelIndex < NUMPIXELS) {
+            strip.setPixelColor(pixelIndex, color);
+        }
+    }
+}
+     
